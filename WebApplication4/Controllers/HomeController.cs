4aa6a9ab06256ffc91a7008be59b7e67d001b0bf
Ann <istomina4815162342@yandex.ru>
2018-02-12 17:55:56 +0300

    initial commit

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4;
+using WebApplication4.Models;
+using System.Data.Entity;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        Context db = new Context();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult AboutRunners()
+        //{
+        //    //var Items = db.Runners;
+        //    //return View(Items);
+        //    var players = db.Runners.Include(p => p.User);
+        //    return View(players.ToList());
+        //}
+
+        public ActionResult Page1()
+        {
+            return View();
+        }
+        public ActionResult Page2()
+        {
+            return View();
+        }
+        public ActionResult Page3()
+        {
+            return View();
+        }
+        public ActionResult Page4()
+        {
+            return View();
+        }
+        //public ActionResult About(int item_id)
+        //{
+        //    var Item = db.Runners.FirstOrDefault(x => x.RunnerId == item_id);
+
+        //    return View(Item);
+        //}
+
+
+    }
+}
---
+++ WebApplication4/Controllers/RunnersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+//using WebApplication4.DAL;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class RunnersController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Runners
+        public ActionResult Index()
+        {
+            var Runners = db.Runners.Include(r => r.User);
+            return View(Runners.ToList());
+        }
+
+        // GET: Runners/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Runner runner = db.Runners.Find(id);
+            if (runner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(runner);
+        }
+
+        // GET: Runners/Create
+        public ActionResult Create()
+        {
+            ViewBag.Email = new SelectList(db.Users, "Email", "Password");
+            return View();
+        }
+
+        // POST: Runners/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RunnerId,Email,Gender,DateOfBirth,Country")] Runner runner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Runners.Add(runner);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Email = new SelectList(db.Users, "Email", "Password", runner.Email);
+            return View(runner);
+        }
+
+        // GET: Runners/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Runner runner = db.Runners.Find(id);
+            if (runner == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Email = new SelectList(db.Users, "Email", "Password", runner.Email);
+            return View(runner);
+        }
+
+        // POST: Runners/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RunnerId,Email,Gender,DateOfBirth,Country")] Runner runner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(runner).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Email = new SelectList(db.Users, "Email", "Password", runner.Email);
+            return View(runner);
+        }
+
+        // GET: Runners/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Runner runner = db.Runners.Find(id);
+            if (runner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(runner);
+        }
+
+        // POST: Runners/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Runner runner = db.Runners.Find(id);
+            db.Runners.Remove(runner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
